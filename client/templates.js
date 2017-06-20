03a24d64e23a749cab6104c4748ae4519d6b20df
hebuer <zhuoyuan@brandeis.edu>
2017-06-19 20:08:49 -0400

    revise for PA1

--- client/templates.js
+++ client/templates.js
@@ -1,31 +1,32 @@
 
-  Template.show_comments.helpers({
-    commentslist() {return Comments.find()},
-  })
-
-  Template.add_comments.events({
-    'click button'(elt,instance) {
-      const username = instance.$('#username').val();
-      const comments = instance.$('#comments').val();
-      console.log('adding '+username);
-      instance.$('#username').val("");
-      instance.$('#comments').val("");
-      Comments.insert({username:username,comments:comments,
-      owner:Meteor.userId(), createAt: new Date });
-      //People.insert({name,birthyear})
-    }
-  })
-
-  template.comments_row.helpers({
-    isOwner() {console.dir(this);
-      return this.comments.owner==Meteor.userId()}
-  })
-  Template.comments_row.events({
-      'click span'(elt,instance) {
-        console.dir(this);
-        console.log(this);
-        console.log(this.person._id);
-        if(this.comments.owner==Meteor.userId()){  Comments.remove(this.person._id);}
-
-      }
-  })
+  // Template.show_comments.helpers({
+  //   commentslist() {return Comments.find()},
+  // })
+  //
+  // Template.add_comments.events({
+  //   'click button'(elt,instance) {
+  //     const username = instance.$('#username').val();
+  //     const comments = instance.$('#comments').val();
+  //     console.log('adding '+username);
+  //     instance.$('#username').val("");
+  //     instance.$('#comments').val("");
+  //     Comments.insert({username:username,comments:comments,
+  //     owner:Meteor.userId(), createAt: new Date });
+  //     //People.insert({name,birthyear})
+  //   }
+  // })
+  //
+  // template.comments_row.helpers({
+  //   isOwner() {console.dir(this);
+  //     return this.comments.owner==Meteor.userId()}
+  // })
+  // Template.comments_row.events({
+  //     'click span'(elt,instance) {
+  //       console.dir(this);
+  //       console.log(this);
+  //       console.log(this.person._id);
+  //       if(this.comments.owner==Meteor.userId()){  Comments.remove(this.person._id);}
+  //
+  //     }
+  // })
+-->
