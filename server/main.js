985c45655f03fbba0154df529837b8ecd2b8a8c6
hebuer <zhuoyuan@brandeis.edu>
2017-06-20 11:39:42 -0400

    remove auto-insert comment

--- server/main.js
+++ server/main.js
@@ -3,6 +3,5 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
     // clear the database
-  Comments.insert({username:'Tim',comments:'not bad'});
-  Comments.insert({username:'Caitlin',comments:'ok'});
+
 });
