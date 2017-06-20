12fa975b819135dbae3bc3210afe669a0a355014
hebuer <zhuoyuan@brandeis.edu>
2017-06-20 10:48:57 -0400

    PA2
    Change the review page and have not yet added editing and deleting feature

--- pages/reviews/client/reviews.js
+++ pages/reviews/client/reviews.js
@@ -1,10 +1,30 @@
-const prevreview =
-  [
-    {name:"Zhuoyuan He"    , email: "zhuoyuan@brandeis.edu" , title: "still working on it" ,comments:"more details are coming soon" },
-  ];
+Template.show_comments.helpers({
+  commentslist() {return Comments.find()},
+})
 
-Template.reviews.helpers(
- {
-   prevreview
- }
-)
+Template.add_comments.events({
+  'click button'(elt,instance) {
+    const username = instance.$('#username').val();
+    const comments = instance.$('#comments').val();
+    console.log('adding '+username);
+    instance.$('#username').val("");
+    instance.$('#comments').val("");
+    Comments.insert({username:username,comments:comments,
+    owner:Meteor.userId(), createAt: new Date });
+    //People.insert({name,birthyear})
+  }
+})
+
+template.comments_row.helpers({
+  isOwner() {console.dir(this);
+    return this.comments.owner==Meteor.userId()}
+})
+Template.comments_row.events({
+     'click'(elt,instance){
+      console.dir(this);
+      console.log(this.comments._id);
+      Comments.remove(this.comments._id);
+      //if(this.comments.owner==Meteor.userId()){  Comments.remove(this.person._id);}
+
+    }
+})
