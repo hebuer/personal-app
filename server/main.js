ef2c563495449c182783a528137ff6bd3b647ca4
hebuer <zhuoyuan@brandeis.edu>
2017-06-20 11:28:28 -0400

    remove Comments.remove
    remove some bug

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  Comments.remove({});  // clear the database
+    // clear the database
   Comments.insert({username:'Tim',comments:'not bad'});
   Comments.insert({username:'Caitlin',comments:'ok'});
 });
