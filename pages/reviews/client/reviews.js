7d6bd3d27a4a971ee96d496308ab744b313e23fe
hebuer <zhuoyuan@brandeis.edu>
2017-06-20 14:18:46 -0400

    removed insecured package and get delete to work

--- pages/reviews/client/reviews.js
+++ pages/reviews/client/reviews.js
@@ -9,22 +9,28 @@ Template.add_comments.events({
     console.log('adding '+username);
     instance.$('#username').val("");
     instance.$('#comments').val("");
-    Comments.insert({username:username,comments:comments,
-    owner:Meteor.userId(), createAt: new Date });
+    var com ={ username:username,
+                comments:comments,
+                owner:Meteor.userId()
+              };
+    Meteor.call('com.insert',com);
     //People.insert({name,birthyear})
   }
 })
 
-template.comments_row.helpers({
-  isOwner() {console.dir(this);
+Template.comments_row.helpers({
+  owner() {console.dir(this);
     return this.comments.owner==Meteor.userId()}
 })
 Template.comments_row.events({
-     'click'(elt,instance){
+     'click span'(elt,instance){
       console.dir(this);
+      console.log(this);
       console.log(this.comments._id);
-      Comments.remove(this.comments._id);
-      //if(this.comments.owner==Meteor.userId()){  Comments.remove(this.person._id);}
-
+      Meteor.call('com.remove',this.comments);
+      // Comments.remove(this.comments._id);
+      // if(this.comments.owner==Meteor.userId()){
+      //   Comments.remove(this.person._id);
+      // }
     }
 })
